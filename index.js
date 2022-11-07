3be40ecf562621280f244e03a8d9bf46ed256f14
codernoyan <coder.noyan@gmail.com>
2022-11-08 00:36:03 +0600

    server created

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const colors = require('colors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Cloud Kitchen server is running');
+});
+
+app.listen(port, () => {
+  console.log(`Server is runiing on port: ${port}`.cyan.bold);
+})

86507bb19f9c8375750c4cfaa1b1c8f91fd61606
codernoyan <coder.noyan@gmail.com>
2022-11-08 03:08:51 +0600

    mongodb connected with server

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const colors = require('colors');
 require('dotenv').config();
 const app = express();
@@ -7,6 +8,21 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// mongodb
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const dbConnect = async () => {
+  try {
+    await client.connect();
+    console.log('MongoDB Connected'.yellow.bold);
+  } catch (error) {
+    console.log(error.name, error.message);
+  }
+}
+dbConnect();
 
 app.get('/', (req, res) => {
   res.send('Cloud Kitchen server is running');

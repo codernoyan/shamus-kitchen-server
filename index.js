1f7f0d6ff9dfd08e33f9a05feb43598556350937
codernoyan <coder.noyan@gmail.com>
2022-11-11 10:01:39 +0600

    removed jwt for fix errors and added update api

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
@@ -10,30 +9,8 @@ app.use(cors());
 app.use(express.json());
 
 // mongodb
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ufdxsbo.mongodb.net/?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-const client = new MongoClient(uri, { useUnifiedTopology: true}, { useNewUrlParser: true }, { connectTimeoutMS: 30000 }, { keepAlive: 1});
-
-// user verify
-const verifyJWT = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).send({
-      message: 'Unauthorized Access'
-    })
-  }
-  const token = authHeader.split(' ')[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(403).send({
-        message: 'Unauthorized Access'
-      })
-    }
-    req.decoded = decoded;
-    next();
-  })
-}
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 const dbConnect = async () => {
   try {
@@ -153,15 +130,8 @@ app.post('/reviews', async (req, res) => {
 })
 
 // get reviews
-app.get('/reviews', verifyJWT, async (req, res) => {
+app.get('/reviews', async (req, res) => {
   try {
-    const decoded = req.decoded;
-    if (decoded.email !== req.query.email) {
-      res.status(403).send({
-        message: 'Unauthorized Access'
-      })
-    }
-
     let query = {};
     if (req.query.email) {
       query = {
@@ -178,6 +148,25 @@ app.get('/reviews', verifyJWT, async (req, res) => {
     });
   }
 });
+
+app.get('/reviews/:id', async (req, res) => {
+  try {
+    const id = req.params.id;
+    const query = { _id: ObjectId(id) };
+    const result = await reviewsCollection.findOne(query);
+
+    res.send({
+      success: true,
+      message: 'Successfully got the requested data',
+      data: result
+    })
+  } catch (error) {
+    res.send({
+      success: false,
+      error: error.message
+    });
+  }
+})
 
 // delete review
 app.delete('/reviews/:id', async (req, res) => {
@@ -229,14 +218,6 @@ app.patch('/reviews/:id', async (req, res) => {
       error: error.message
     });
   }
-});
-
-// jwt post api
-app.post('/jwt', (req, res) => {
-  const user = req.body;
-  console.log(user);
-  const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' })
-  res.send({ token });
 });
 
 app.get('/', (req, res) => {
